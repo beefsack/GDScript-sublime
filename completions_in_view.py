505db87e8b87cf84837ba16ef181de3e7ac8b482
Gen <z.rz2323721@gmail.com>
2014-11-21 14:37:46 +0800

    Update

--- completions_in_view.py
+++ completions_in_view.py
@@ -1,12 +1,18 @@
 import sublime, sublime_plugin
 import string
 
-class PleasurazyAPICompletionsPackageEventListener(sublime_plugin.EventListener):
+class CompletionsInViewEventListener(sublime_plugin.EventListener):
 
   def on_query_completions(self, view, prefix, locations):
     self.completions = []
     window = sublime.active_window()
-    compDefault = [v.extract_completions(prefix) for v in window.views()]
-    compDefault = [(item, item) for sublist in compDefault for item in sublist]
-    compDefault = list(set(compDefault))
-    return (compDefault)
+    for v in window.views():
+    	arr = v.extract_completions(prefix)
+    	for com in arr:
+    		if not com in self.completions:
+    			self.completions.append(com) 
+
+    self.completions = [self.completions]
+    self.completions = [(item, item) for sublist in self.completions for item in sublist]
+    self.completions = list(set(self.completions))
+    return (self.completions)
