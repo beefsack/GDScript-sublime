e6d1e05eddd71651ee70920ebf3ffff784944d1a
Joe Ulfers <ju@josiahulfers.com>
2019-11-14 13:02:15 -0500

    Dry identifier matching

---
+++ syntax_test_gdscript.gd
@@ -0,0 +1,9 @@
+# SYNTAX TEST "Packages/User/GDScript-sublime/GDScript.sublime-syntax"
+
+
+
+func foo() -> A:
+# <- storage.type.function.gdscript
+#    ^^^ entity.name.function.gdscript
+    pass
+#   ^^^^ keyword.control.flow.gdscript
